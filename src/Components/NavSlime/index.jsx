dd8055e16de44974ac5821f2da154a0c31a8db4f
Enzorieldev <enzorieldev@outlook.com>
2024-09-24 18:01:07 -0300

    Styling

--- src/Components/NavSlime/index.jsx
+++ src/Components/NavSlime/index.jsx
@@ -5,26 +5,18 @@ const NavSlime = () => {
     <div className={styles.contenedor}>
       <section className={styles.slimeNavbar}>
         <ul>
-          <li>
-            <img src="NavbarSlime.svg" alt="Descripción del SVG" />
-            <a href="#home">Inicio</a>
-          </li>
-          <li>
-            <img src="NavbarSlime.svg" alt="Descripción del SVG" />
-            <a href="#projects">Proyectos</a>
-          </li>
-          <li>
-            <img src="NavbarSlime.svg" alt="Descripción del SVG" />
-            <a href="#technologies">Tecnologías</a>
-          </li>
-          <li>
-            <img src="NavbarSlime.svg" alt="Descripción del SVG" />
-            <a href="#about">Sobre mí</a>
-          </li>
-          <li>
-            <img src="NavbarSlime.svg" alt="Descripción del SVG" />
-            <a href="#contact">Contacto</a>
-          </li>
+          <a href="#home">
+            <li>Inicio</li>
+          </a>{" "}
+          <a href="#projects">
+            <li>Proyectos</li>
+          </a>
+          <a href="#technologies">
+            <li>Tecnologías</li>
+          </a>
+          <a href="#contact">
+            <li>Contacto</li>
+          </a>
         </ul>
       </section>
     </div>
